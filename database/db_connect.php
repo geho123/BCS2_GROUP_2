7ed389ff4e18ec40e7eeb3f9e367b37068ffbb83
geho123 <gehojoseph58@gmail.com>
2024-12-11 13:04:29 +0300

    First VERSION of social network project registration not done yet!

---
+++ database/db_connect.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "social";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) { 
+    die("ERROR: Could not connect " . $conn->connect_error);
+}
+
+?>
